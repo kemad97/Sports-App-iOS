09d97f137729bf9444d92f895c5984b9ce826ef4
Mustafaa-Hussain <mustafahsan3@gmail.com>
2025-05-12 15:31:30 +0300

    add sports leagues package

---
+++ Sports App/sports_leagues/SportsLeaguesViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SportsLeaguesViewController.swift
+//  Sports App
+//
+//  Created by Mustafa Hussain on 11/05/2025.
+//
+
+import UIKit
+
+class SportsLeaguesViewController: UIViewController {
+    var sportName : String!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print(sportName!)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
