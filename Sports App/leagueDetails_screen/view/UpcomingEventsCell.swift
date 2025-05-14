04293191305e8337751f69a34068b6db1fd36262
kemad97 <kerolos.emad992@gmail.com>
2025-05-14 12:25:38 +0300

    create league files

---
+++ Sports App/leagueDetails_screen/view/UpcomingEventsCell.swift
@@ -0,0 +1,24 @@
+//
+//  UpcomingEventsCell.swift
+//  Sports App
+//
+//  Created by Kerolos on 14/05/2025.
+//
+
+import UIKit
+
+class UpcomingEventsCell: UICollectionViewCell {
+    @IBOutlet weak var homeName: UILabel!
+
+    @IBOutlet weak var awayName: UILabel!
+    
+    @IBOutlet weak var date: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var homeImg: UIImageView!
+    @IBOutlet weak var awayImg: UIImageView!
+    
+    
+    
+    
+    
+}
